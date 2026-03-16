4e334354771dda18ddea0bfd0714b11f2350d68a
huok <77536370+vraestoren@users.noreply.github.com>
2026-03-16 17:51:37 +0300

    Update bingo.sh

--- src/bingo.sh
+++ src/bingo.sh
@@ -1,83 +1,46 @@
 #!/bin/bash
 
+params=null
 api="https://invest.divweb.ru/api"
-sign=null
-vk_user_id=null
-vk_ts=null
-vk_ref=null
 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
-function authenticate() {
-	# 1 - sign: (string): <sign>
-	# 2 - vk_user_id: (integer): <vk_user_id>
-	# 3 - vk_ts: (integer): <vk_ts>
-	# 4 - vk_ref: (string): <vk_ref>
-	# 5 - access_token_settings: (string): <access_token_settings - default: >
-	# 6 - are_notifications_enabled: (integer): <are_notifications_enabled: default: 0>
-	# 7 - is_app_user: (integer): <is_app_user - default: 0>
-	# 8 - is_favorite: (integer): <is_favorite - default: 0>
-	# 9 - language: (string): <language - default: ru>
-	# 10 - platform: (string): <platform - default: desktop_web>
-	sign=$1
-	vk_user_id=$2
-	vk_ts=$3
-	vk_ref=$4
-	params="vk_access_token_settings=${5:-}&vk_app_id=7766223&vk_are_notifications_enabled=${6:-0}&vk_is_app_user=${7:-0}&vk_is_favorite=${8:-0}&vk_language=${9:-ru}&vk_platform=${10:-desktop_web}&vk_ref=$vk_ref&vk_ts=$vk_ts&vk_user_id=$vk_user_id&sign=$sign"
-	echo $params
+authenticate() {
+    params="vk_access_token_settings=${5:-}&vk_app_id=7766223&vk_are_notifications_enabled=${6:-0}&vk_is_app_user=${7:-0}&vk_is_favorite=${8:-0}&vk_language=${9:-ru}&vk_platform=${10:-desktop_web}&vk_ref=$4&vk_ts=$3&vk_user_id=$2&sign=$1"
 }
 
-function app_init() {
-	curl --request GET \
-		--url "$api/init/" \
-		--user-agent "$user_agent" \
-		--header "xvk: $params"
+_get() {
+    curl -s -X GET "$api/$1"    \
+        -H "User-Agent: $user_agent" \
+        -H "xvk: $params"
 }
 
-function get_popular() {
-	curl --request GET \
-		--url "$api/getPopular/" \
-		--user-agent "$user_agent" \
-		--header "xvk: $params"
+_post() {
+    curl -s -X POST "$api/$1"   \
+        -H "User-Agent: $user_agent" \
+        -H "xvk: $params"       \
+        -d "$2"
 }
 
-function get_new() {
-	curl --request GET \
-		--url "$api/getNew/" \
-		--user-agent "$user_agent" \
-		--header "xvk: $params"
+app_init() {
+    _get "init/"
 }
 
-function like_bingo() {
-	# 1 - bingo_id: (string): <bingo_id>
-	# 2 - like: (boolean): <true, false - default: true>
-	curl --request POST \
-		--url "$api/like/$1" \
-		--user-agent "$user_agent" \
-		--header "xvk: $params" \
-		--data '{
-			"like": '${2:-true}'
-		}'
+get_popular() {
+    _get "getPopular/"
 }
 
-
-function report_bingo() {
-	# 1 - bingo_id: (string): <bingo_id>
-	# 2 - reason: (string): <reason>
-	# 3 - comment: (string): <comment - default: "">
-	curl --request POST \
-		--url "$api/claim/$1" \
-		--user-agent "$user_agent" \
-		--header "xvk: $params" \
-		--data '{
-			"reason": "'$2'",
-			"comment": '${3:-""}'
-		}'
+get_new() {
+    _get "getNew/"
 }
 
-function delete_bingo() {
-	# 1 - bingo_id: (string): <bingo_id>
-	curl --request GET \
-		--url "$api/delete/$1" \
-		--user-agent "$user_agent" \
-		--header "xvk: $params"
+like_bingo() {
+    _post "like/$1" '{"like": '${2:-true}'}'
+}
+
+report_bingo() {
+    _post "claim/$1" '{"reason": "'$2'", "comment": "'${3:-}'"}'
+}
+
+delete_bingo() {
+    _get "delete/$1"
 }
